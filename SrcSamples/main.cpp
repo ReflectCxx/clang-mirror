8f076d2d7d9e64c94d7abb050341f3ecd371f17a
neeraj31285 <neeraj.singh31285@gmail.com>
2026-02-09 13:35:34 +0530

    bootstrap: in progress.

--- SrcSamples/main.cpp
+++ SrcSamples/main.cpp
@@ -6,19 +6,34 @@
 int main(int argc, const char** argv)
 {
 	{
-		auto revStr = cxx::mirror().getFunction(cxx::fn::reverseString::id).value();
+		auto classId = cxx::type::nsdate::Date::id;
 
-		auto functor = revStr.argsT().returnT<std::string>();
-
-		auto str = functor();
-	} {
-		auto class_ = cxx::mirror().getRecord(cxx::type::nsdate::Date::id);
+		auto class_ = cxx::mirror().getRecord(classId);
+		if (!class_) {
+			std::cout << "\nType "<< classId << " not found." << std::endl;
+			return 0;
+		}
 
 		auto [err, dateObj] = class_->ctorT()(rtl::alloc::Stack);
+		if (err != rtl::error::None) {
+			std::cout << "\nObject construction failed." << std::endl;
+			return 0;
+		}
 
-		auto methodMeta = class_->getMethod(cxx::type::nsdate::Date::fn::getAsString::id);
+		auto methodId = cxx::type::nsdate::Date::fn::getAsString::id;
+
+		auto methodMeta = class_->getMethod(methodId);
+		if (!methodMeta) {
+			std::cout << "\nMethod " << methodId << " not found." << std::endl;
+			return 0;
+		}
 
 		auto getAsString = methodMeta->targetT().argsT().returnT<std::string>();
+
+		if (!getAsString) {
+			std::cout << "\nMethod didn't resolve. error : " << rtl::to_string(getAsString.get_init_error()) << std::endl;
+			return 0;
+		}
 
 		auto [err0, str] = getAsString(std::cref(dateObj))();
 		std::cout << "\nReturned value : " << *str << std::endl;
